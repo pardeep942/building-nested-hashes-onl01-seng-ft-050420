ad62898016a2ad0523347451ef36afa4150d296c
PARDEEP KAUR <pkaur84@aol.com>
2020-05-16 02:00:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,6 @@ def bonus
   }
 
   #code your solution here:
-  it "uses the []= method to change the hero and heroine's status from alive to dead" do 
   
   expect(bonus[:montague][:hero][:status]).to eq("dead")
     expect(bonus[:capulet][:heroine][:status]).to eq("dead")	    
